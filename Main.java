0e30f2362151128a4be2b6ef982d2a0c76ef6de9
Oppermann <cra-dgd-u1@fh-h.de>
2023-05-10 16:54:27 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -8,4 +8,12 @@
 
 public class Main
 {
+    public static void main (String args[])
+    {
+        int summe = 0;
+        for (int i = 1; i <= 10; ++i)
+        {
+            summe = summe + i;
+        }
+    }
 }
